a966ef5061406bd69ef6d7e60b12e9bda119e2cc
Henry Otieno <henrynyobange@gmail.com>
2023-03-03 04:51:53 +0300

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,37 @@
-#include "main.h"
 /**
- * *leet - encodes a string into 1337
- * @: pointer to the string to be encoded
+ * leet - a function that encodes a string into 1337
  *
- * Return: pointer to the encoded string
- */
-char *leet(char *)
+ * @s: string input
+ *
+ * Return: @s
+*/
+
+char *leet(char *s)
 {
+	int i, c = 0;
+	int sl[] = {97, 101, 111, 116, 108};
+	int ul[] = {65, 69, 79, 84, 76};
+	int n[] = {52, 51, 48, 55, 49};
+
+	/*iterate values in array s*/
+	while (s[c] != '\0')
+	{
+		/**
+		 * loop through array value five times
+		 * to check if the value is equal to
+		 * any small letter in array sl or
+		 * upper letter in array ul and if it
+		 * is replace it with the value in array n
+		*/
+		for (i = 0; i < 5; i++)
+		{
+			if (s[c] == sl[i] || s[c] == ul[i])
+			{
+				s[c] = n[i];
+				break;
+			}
+		}
+		c++;
+	}
+	return (s);
 }
